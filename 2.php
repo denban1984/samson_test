73757ee74708deefe504c18a3a82120f417038e8
denban <denban@mail.ru>
2020-12-03 00:48:55 +0300

    step2/exportXml

--- 2.php
+++ 2.php
@@ -160,5 +160,193 @@ function  importXml($XmlFile){
  }
 
 
+$ErrorArray=array();
+
+
+function ProductSearch($connect, $CodeRubricArray){
+  /*получили список всех рубрк теперь нужен список всех товаров входящих в эти рубрики
+  При этом учтено что один товар может входить в неограниченное количество рубрик
+  Для этих целей пройдемся по таблице a_product_category */
+ $ResultArray = array();
+ foreach ($CodeRubricArray as $Code){
+   try{
+    
+    $sql = mysqli_prepare($connect, 'SELECT PRODUCT_CODE FROM a_product_category WHERE CODE_RUBRIC=?');
+    mysqli_stmt_bind_param($sql , "i", $Code);
+    mysqli_stmt_execute($sql);
+    $result = mysqli_stmt_get_result($sql);
+
+      while ($ResultRows = mysqli_fetch_array($result, MYSQLI_NUM))
+        {
+            foreach ($ResultRows as $Row) { 
+              
+              $ResultArray[$Row][]= $Code; 
+            }     
+        }
+
+
+      }catch(Exception $e){$ErrorArray[] = 'RecursivaSearch Line 19  ERROR MSG: '. $e->getMessage();}
+    
+     }
+
+    /*На выходе получаем массив в котором ключ элемента это код товара, а значения каждого элемента - массив, содержащий коды рубрик в которых этот товар найден */
+  return $ResultArray;
+}
+
+
+function RecursivSearch($connect, $CodeRubric){
+  /*так как по заданию глубина вложения рубрикатора произвольная, то для полного перебора вложений рубрикатора используется рекурсия*/
+    $ResultArray = array();
+    $TmpResultArray = array();
+    $ResultArray[]=$CodeRubric;
+ try{
+
+  /*в таблице a_category_chain ищем "дочерние" рубрики для "родительской" рубрики переданой через $CodeRubric */
+  $sql = mysqli_prepare($connect, 'SELECT CODE_RUBRIC FROM a_category_chain WHERE CODE_PARENT_RUBRIC=?');
+  mysqli_stmt_bind_param($sql , "i", $CodeRubric);
+  mysqli_stmt_execute($sql);
+  $result = mysqli_stmt_get_result($sql);
+
+    while ($ResultRows = mysqli_fetch_array($result, MYSQLI_NUM))
+        {
+            foreach ($ResultRows as $Row) { $TmpResultArray[] = $Row; }     
+        }
+
+   if (count($TmpResultArray)>0){
+     
+     /*если "дочерние" рубрики найдены то начинается их рекурсивный обход*/
+    foreach($TmpResultArray as $value)
+        {
+           /*рекурсивный вызов тут*/
+          $ResultArray = array_merge($ResultArray,RecursivSearch($connect, $value));
+        }
+     }
+
+  }catch(Exception $e){$ErrorArray[] = 'RecursivaSearch Line 19  ERROR MSG: '. $e->getMessage();}
+    
+    asort($ResultArray);
+  return $ResultArray;
+}
+
+
+function SaveToXml($connect, $XmlFilePath, $ProductArray){
+
+ if(dirname($XmlFilePath)!='.'){} 
+  else{ 
+    $XmlFilePath = __DIR__.'\\'.$XmlFilePath;}
+ try{
+ $File = fopen($XmlFilePath, "w");
+ fclose($File);
+ }catch(Exception $e){$ErrorArray[] = ' Line 83 ERROR MSG: '. $e->getMessage(); }
+ try{
+ $ExportFile = file_get_contents($XmlFilePath);
+ $ExportFile .= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>".PHP_EOL;
+ $ExportFile .= PHP_EOL;
+ $ExportFile .= "<Товары>".PHP_EOL;
+ $ExportFile .= PHP_EOL;
+ //основной перебор
+ foreach ($ProductArray as $ProductCode => $RubricArray) {
+ 
+ //получаем название товара 
+ $sql = mysqli_prepare($connect, 'SELECT PRODUCT_NAME FROM a_product WHERE PRODUCT_CODE=?');
+    mysqli_stmt_bind_param($sql , "i", $ProductCode);
+    mysqli_stmt_execute($sql);
+    mysqli_stmt_bind_result($sql, $result);
+    mysqli_stmt_fetch($sql);
+    mysqli_stmt_close($sql);
+  $ExportFile .= "<Товар Код=\"".$ProductCode."\" Название=\"".$result."\">".PHP_EOL;
+  $ExportFile .= PHP_EOL;
+
+ //Получаем цены 
+ $sql = mysqli_prepare($connect, "SELECT a_price_type.PRICE_TYPE, a_price.PRICE FROM a_price_type INNER JOIN a_price ON(a_price_type.ID=a_price.PRICE_TYPE_ID)  WHERE a_price.PRODUCT_CODE=?");
+   
+    mysqli_stmt_bind_param($sql , "i", $ProductCode);
+    mysqli_stmt_execute($sql);
+    $result = mysqli_stmt_get_result($sql);
+  
+      while ($ResultRows = mysqli_fetch_array($result, MYSQLI_NUM))
+        {
+            for($i=0; $i<=count($ResultRows)-1;$i++)  {
+              $ExportFile .= "<Цена Тип=\"".$ResultRows[$i]."\">";
+              $i++;
+              $ExportFile .= $ResultRows[$i]."</Цена>".PHP_EOL;
+              $ExportFile .= PHP_EOL;
+            }
+        }
+    mysqli_stmt_close($sql);
+ 
+ //Получаем свойства
+  $ExportFile .= "<Свойства>".PHP_EOL;
+  $ExportFile .= PHP_EOL;
+    $sql = mysqli_prepare($connect, 'SELECT PRODUCT_PROPERTY, PROPERTY_VALUE  FROM a_property WHERE PRODUCT_CODE=?');
+   
+    mysqli_stmt_bind_param($sql , "i", $ProductCode);
+    mysqli_stmt_execute($sql);
+    $result = mysqli_stmt_get_result($sql);
+  
+      while ($ResultRows = mysqli_fetch_array($result, MYSQLI_NUM))
+        {
+            for($i=0; $i<=count($ResultRows)-1;$i++)  {
+              $NameProperty = $ResultRows[$i];
+              $ExportFile .= "<".$NameProperty.">";
+              $i++;
+              $ExportFile .= $ResultRows[$i]."</".$NameProperty.">".PHP_EOL;
+              $ExportFile .= PHP_EOL;
+            }
+        }
+    mysqli_stmt_close($sql);
+  $ExportFile .= "</Свойства>".PHP_EOL;  
+  $ExportFile .= PHP_EOL;
+
+ //Получаем разделы
+ $ExportFile .= "<Разделы>".PHP_EOL;
+ $ExportFile .= PHP_EOL;
+ $sql = mysqli_prepare($connect, "SELECT a_category.NAME_RUBRIC FROM a_category INNER JOIN a_product_category ON(a_category.CODE_RUBRIC=a_product_category.CODE_RUBRIC)  WHERE a_product_category.PRODUCT_CODE=?");
+   
+    mysqli_stmt_bind_param($sql , "i", $ProductCode);
+    mysqli_stmt_execute($sql);
+    $result = mysqli_stmt_get_result($sql);
+  
+      while ($ResultRows = mysqli_fetch_array($result, MYSQLI_NUM))
+        {
+            foreach ($ResultRows as $Row) { 
+              
+              $ExportFile .="<Раздел>".$Row."</Раздел>".PHP_EOL; 
+              $ExportFile .= PHP_EOL;
+            }   
+        }
+    mysqli_stmt_close($sql);
+
+ $ExportFile .= "</Разделы>".PHP_EOL; 
+ $ExportFile .= PHP_EOL; 
+ $ExportFile .= "</Товар>".PHP_EOL;
+ $ExportFile .= PHP_EOL;
+ }
+ $ExportFile .= "</Товары>".PHP_EOL;
+ $ExportFile .= PHP_EOL;
+ file_put_contents($XmlFilePath, $ExportFile);
+ }catch(Exception $e){$ErrorArray[] = ' Line 98 ERROR MSG: '. $e->getMessage(); }
+}
+
+
+function  exportXml($XmlFilePath,$CodeRubric){
+ $Result = FALSE; 
+ $ErrorArray[]=$XmlFilePath;
+ $connect = mysqli_connect(HOST, USER, PASSWORD, DATABASE); 
+    if (!$connect) {
+    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+    }else{
+
+   /*Каскадный вызов  RecursivSearch -> ProductSearch -> SaveToXml
+    В этих трех функциях реализована вся логика работы моей функции exportXml */
+  SaveToXml($connect, $XmlFilePath , ProductSearch($connect, RecursivSearch($connect, $CodeRubric) ) );
+ 
+  } 
+}
+
+
 
 ?>
